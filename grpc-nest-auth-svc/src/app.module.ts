551c2d521ee0cc54ab6447af4190aa531fe6c0ff
Alvaro Aguinaga <alvarodaniel2808@gmail.com>
2022-10-06 22:02:36 -0500

    microservice gateway & microservice auth

--- grpc-nest-auth-svc/src/app.module.ts
+++ grpc-nest-auth-svc/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 7432,
+      database: 'micro_auth',
+      username: 'wala',
+      password: '12345',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
